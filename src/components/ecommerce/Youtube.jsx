fe579f3d180a0a26b5b8869671ab4c8ad993440d
Ashikuzzaman Moon <ashikuzzamanmoon31@gmail.com>
2025-04-28 09:29:00 +0600

    featuress

--- src/components/ecommerce/Youtube.jsx
+++ src/components/ecommerce/Youtube.jsx
@@ -4,8 +4,9 @@ import LandingButton from "./LandingButton";
 const Youtube = ({ scrollToRef }) => {
   const handleScroll = () => {
     if (scrollToRef?.current) {
-      const topOffset = scrollToRef.current.getBoundingClientRect().top + window.pageYOffset;
-        const offset = -160;
+      const topOffset =
+        scrollToRef.current.getBoundingClientRect().top + window.pageYOffset;
+      const offset = -160;
 
       window.scrollTo({
         top: topOffset + offset,
@@ -18,12 +19,10 @@ const Youtube = ({ scrollToRef }) => {
     <div className="">
       <div className="container pb-5 lg:pb-10 pt-20">
         <h1 className="text-2xl md:text-3xl font-bold text-center px-2 md:px-10 py-4  bg-[#f1593a] text-white rounded-md shadow-lg shadow-slate-600">
-          'Admin Panel'-এর  ডেমো দেখুন
+          'Admin Panel'-এর ডেমো দেখুন 
         </h1>
         <div>
-          <h2 className="font-bold text-2xl md:text-4xl text-center my-10">
-            
-          </h2>
+          <h2 className="font-bold text-2xl md:text-4xl text-center my-10"></h2>
           <div className="container flex justify-center my-10">
             <div className="w-full md:w-2/3 aspect-w-16 aspect-h-9 border-[#f1593a] border-8 rounded-2xl">
               <iframe
