c615c0116ad98d75541be6721928a11545af437e
Hasib0607 <hasib.soyad@gmail.com>
2025-05-12 17:31:13 +0600

    tag maanage

--- src/utils/GoogleTagManager.jsx
+++ src/utils/GoogleTagManager.jsx
@@ -3,15 +3,34 @@ import Script from "next/script";
 const GoogleTagManager = () => {
   return (
     <>
-      <script
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17074663724"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NNL4WP82');
-            `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17074663724');
+          `,
+        }}
+      />
+
+      {/* Event snippet for Page view conversion */}
+      <Script
+        id="gtag-conversion"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            gtag('event', 'conversion', {
+              'send_to': 'AW-17074663724/w-MuCLSdpsYaEKzi6s0_'
+            });
+          `,
         }}
       />
     </>
