2b7455ab48fd9bd64b5c1f47b07737325cd685fa
Hasib0607 <hasib.soyad@gmail.com>
2025-05-12 17:41:09 +0600

    snipet

--- src/utils/GoogleTagManager.jsx
+++ src/utils/GoogleTagManager.jsx
@@ -21,15 +21,37 @@ const GoogleTagManager = () => {
         }}
       />
 
-      {/* Event snippet for Page view conversion */}
+      {/* Page view conversion event */}
       <Script
-        id="gtag-conversion"
+        id="gtag-pageview"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             gtag('event', 'conversion', {
               'send_to': 'AW-17074663724/w-MuCLSdpsYaEKzi6s0_'
             });
+          `,
+        }}
+      />
+
+      {/* gtag_report_conversion function for manual conversion tracking */}
+      <Script
+        id="gtag-report-conversion"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            function gtag_report_conversion(url) {
+              var callback = function () {
+                if (typeof(url) != 'undefined') {
+                  window.location = url;
+                }
+              };
+              gtag('event', 'conversion', {
+                'send_to': 'AW-17074663724/EUE7CKbpr8YaEKzi6s0_',
+                'event_callback': callback
+              });
+              return false;
+            }
           `,
         }}
       />
